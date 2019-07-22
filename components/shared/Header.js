d78e2af4aca28d63482d4f2e88e182babb1d2417
simotae14 <startae14@gmail.com>
2019-07-23 00:18:24 +0200

    add navbar reactstrap component

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,41 +1,63 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 // This is the Link API
 import Link from 'next/link';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink } from 'reactstrap';
 
-class Header extends React.Component {
+const BsNavLink = ({ route, title }) => (
+    <Link href={route}>
+        <a className="nav-link"> { title } </a>
+    </Link>
+);
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
     render() {
         return (
-            <Fragment>
-                <Link href="/">
-                    <a style={{ 'fontSize': '20px' }}> Home </a>
-                </Link>
-                <Link href="/about">
-                    <a> About </a>
-                </Link>
-                <Link href="/portfolios">
-                    <a> Portfolio </a>
-                </Link>
-                <Link href="/blogs">
-                    <a> Blog </a>
-                </Link>
-                <Link href="/cv">
-                    <a> Cv </a>
-                </Link>
-                <style jsx>
-                    {
-                        `
-                            a {
-                                font-size: 20px;
-                            };
-                            .customClass {
-                                color: red;
-                            }
-                        `
-                    }
-                </style>
-            </Fragment>
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Simone Taeggi</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <BsNavLink title="Home" route="/" />
+                            </NavItem>
+                            <NavItem>
+                                <BsNavLink title="About" route="/about" />
+                            </NavItem>
+                            <NavItem>
+                                <BsNavLink title="Portfolio" route="/portfolios" />
+                            </NavItem>
+                            <NavItem>
+                                <BsNavLink title="Blog" route="/blogs" />
+                            </NavItem>
+                            <NavItem>
+                                <BsNavLink title="Cv" route="/cv" />
+                            </NavItem>
+
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
         );
     }
 }
-
-export default Header;
